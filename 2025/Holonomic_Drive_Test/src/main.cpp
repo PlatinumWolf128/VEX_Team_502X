efb9a903ca0cb968a0eb18fa988782ee672cce13
Sanjay Nataraj <2886932@edu.burnabyschools.ca>
2025-12-10 15:33:33 -0800

    Added some changes to the holonomic drive code after testing.
    - Mainly, we capped all the different speed values
      to keep them within the correct range.
    - One thing we're probably gonna add next is using
      the inertial sensor to maintain the angle.

--- 2025/Holonomic_Drive_Test/src/main.cpp
+++ 2025/Holonomic_Drive_Test/src/main.cpp
@@ -7,24 +7,48 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
+#include <algorithm>
 
 using namespace vex;
+using std::max;
+using std::min;
+using std::abs;
 
 brain MyMind;
 controller Controller;
 
-motor FrontLeft(PORT12);
-motor FrontRight(PORT13, true);
-motor BackLeft(PORT14);
-motor BackRight(PORT15, true);
+motor FrontLeft(PORT12, ratio36_1);
+motor FrontRight(PORT13, ratio36_1, true);
+motor BackLeft(PORT14, ratio36_1);
+motor BackRight(PORT19, ratio36_1, true);
 
-void drive(int forward, int strafe, int turn) {
+void drive(double forward, double strafe, double turn) {
 
-    // Theoretical holonomic drive formula
-    FrontLeft.spin(fwd, (forward + strafe + turn), velocityUnits::pct);
-    FrontRight.spin(fwd, (forward - strafe - turn), velocityUnits::pct);
-    BackLeft.spin(fwd, (forward - strafe + turn), velocityUnits::pct);
-    BackRight.spin(fwd, (forward + strafe - turn), velocityUnits::pct);
+    if (abs(forward) <= 10) {forward = 0;}
+    if (abs(strafe) <= 10) {strafe = 0;}
+    if (abs(turn) <= 10) {turn = 0;}
+
+    // Holonomic drive formula
+    double frontLeftSpeed = forward + strafe + turn;
+    double frontRightSpeed = forward - strafe - turn;
+    double backLeftSpeed = forward - strafe + turn;
+    double backRightSpeed = forward + strafe - turn;
+
+    // Capping the motor speeds between -100 and +100
+    frontLeftSpeed = max(-100.0, min(frontLeftSpeed, 100.0));
+    frontRightSpeed = max(-100.0, min(frontRightSpeed, 100.0));
+    backLeftSpeed = max(-100.0, min(backLeftSpeed, 100.0));
+    backRightSpeed = max(-100.0, min(backRightSpeed, 100.0));
+
+    if (abs(frontLeftSpeed) <= 10) {FrontLeft.stop(brake);}
+    if (abs(frontRightSpeed) <= 10) {FrontRight.stop(brake);}
+    if (abs(backLeftSpeed) <= 10) {BackLeft.stop(brake);}
+    if (abs(frontRightSpeed) <= 10) {FrontRight.stop(brake);}
+
+    FrontLeft.spin(fwd, frontLeftSpeed, velocityUnits::pct);
+    FrontRight.spin(fwd, frontRightSpeed, velocityUnits::pct);
+    BackLeft.spin(fwd, backLeftSpeed, velocityUnits::pct);
+    BackRight.spin(fwd, backRightSpeed, velocityUnits::pct);
 
 }
 
@@ -32,9 +56,9 @@ int main() {
    
     while(1) {
         
-        int forward = Controller.Axis3.position();
-        int strafe = Controller.Axis4.position();
-        int turn = Controller.Axis1.position();
+        double forward = Controller.Axis3.position();
+        double strafe = Controller.Axis4.position();
+        double turn = Controller.Axis1.position();
 
         drive(forward, strafe, turn);
 
