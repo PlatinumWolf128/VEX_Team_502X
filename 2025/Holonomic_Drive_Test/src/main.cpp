fa0dd961f9610f6cc9f909e8d6f6813b59e5a9b8
PlatinumWolf128 <sanjay.nataraj2009@gmail.com>
2025-12-14 19:49:03 -0800

    Added some extra alignment options.
    - Made it so that in theory, the robot can be
      inverted and made to face the 180 degree mark
      instead of 0. When this happens, the controls
      will also be inverted.
      * This may make it easier to drive the robot
        when it's facing towards the driver.
    - Also made it so that the bot doesn't auto-align
      unless you push a button, and moving the right
      joystick to rotate deliberately disables this
      auto-align mode.

--- 2025/Holonomic_Drive_Test/src/main.cpp
+++ 2025/Holonomic_Drive_Test/src/main.cpp
@@ -17,10 +17,10 @@ using std::abs;
 brain MyMind;
 controller Controller;
 
-motor FrontLeft(PORT12, ratio36_1);
-motor FrontRight(PORT13, ratio36_1, true);
-motor BackLeft(PORT14, ratio36_1);
-motor BackRight(PORT19, ratio36_1, true);
+motor FrontLeft(PORT12, ratio6_1);
+motor FrontRight(PORT13, ratio6_1, true);
+motor BackLeft(PORT14, ratio6_1);
+motor BackRight(PORT19, ratio6_1, true);
 
 inertial Inertial(PORT7);
 
@@ -34,9 +34,15 @@ double previousError;
 double integral;
 double derivative;
 
-double headingMaintainer(double targetHeading, double currentHeading) {
+double aligner(double targetHeading, double currentHeading) {
 
     error = targetHeading - currentHeading;
+
+    // Makes it so that the error is along the shortest possible route to the
+    // target. 
+    // For example, if the target is 0 and your current heading is 270,
+    // you would want the error to be 90 and not -270. This way, you spin 90
+    // degrees clockwise and not 270 degrees counter-clockwise.
     if (error > 180) error -= 360;
     if (error < -180) error += 360;
 
@@ -65,10 +71,11 @@ void drive(double forward, double strafe, double turn) {
     backLeftSpeed = max(-100.0, min(backLeftSpeed, 100.0));
     backRightSpeed = max(-100.0, min(backRightSpeed, 100.0));
 
-    if (fabs(frontLeftSpeed) <= 10) {FrontLeft.stop(brake);}
-    if (fabs(frontRightSpeed) <= 10) {FrontRight.stop(brake);}
-    if (fabs(backLeftSpeed) <= 10) {BackLeft.stop(brake);}
-    if (fabs(frontRightSpeed) <= 10) {FrontRight.stop(brake);}
+    // This segment may not be needed. Do some testing with and without it.
+    if (fabs(frontLeftSpeed) <= 10) FrontLeft.stop(brake);
+    if (fabs(frontRightSpeed) <= 10) FrontRight.stop(brake);
+    if (fabs(backLeftSpeed) <= 10) BackLeft.stop(brake);
+    if (fabs(frontRightSpeed) <= 10) FrontRight.stop(brake);
 
     FrontLeft.spin(fwd, frontLeftSpeed, velocityUnits::pct);
     FrontRight.spin(fwd, frontRightSpeed, velocityUnits::pct);
@@ -83,20 +90,60 @@ int main() {
     wait(2000, msec);
     Inertial.setHeading(0, degrees);
 
+    bool alignWithField = false;
+    bool previousUpPressed = false;
+    bool invert = false;
+
+    double forward = 0;
+    double strafe = 0;
+    double turn = 0;
+    double targetHeading = 0;
+
     while(1) {
 
-        double forward = Controller.Axis3.position();
-        double strafe = Controller.Axis4.position();
-        double turn = Controller.Axis1.position();
+        if (invert) {
+            forward = -Controller.Axis3.position();
+            strafe = -Controller.Axis4.position();
+            turn = -Controller.Axis1.position();
+        } else {
+            forward = Controller.Axis3.position();
+            strafe = Controller.Axis4.position();
+            turn = Controller.Axis1.position();
+        }
         double currentHeading = Inertial.heading(degrees);
 
-        if (fabs(turn) < DEADZONE) {
-            turn = headingMaintainer(0, currentHeading);
-        } else {
-            integral = 0;
-            previousError = 0;
+        // Toggle alignment mode if the up button is pressed.
+        if (Controller.ButtonUp.pressing() == true && previousUpPressed == false) {
+            alignWithField = !alignWithField;
+            invert = false;
         }
 
+        // If the down button is pressed, invert the controls.
+        if (Controller.ButtonDown.pressing() == true) {
+            invert = true;
+        }
+        
+        // If the right joystick (used for rotating) is moved, then the bot
+        // snaps out of auto-alignment mode. If the joystick is not moved and
+        // the up button is toggled, the bot snaps into auto-align mode.
+        if (fabs(turn) > DEADZONE) {
+            // The right joystick is moved
+            alignWithField = false;
+            invert = false;
+            integral = 0;
+            previousError = 0;
+            targetHeading = currentHeading;
+        } else if ((fabs(turn) < DEADZONE) && alignWithField == true) {
+            // The joystick is not being moved and the bot is in alignment mode
+            targetHeading = 0;
+        } else if ((fabs(turn) < DEADZONE) && invert == true) {
+            // The joystick is not being moved and the bot is in inverted mode
+            targetHeading = 180;
+        }
+
+        previousUpPressed = Controller.ButtonUp.pressing();
+
+        turn = aligner(targetHeading, currentHeading);
         drive(forward, strafe, turn);
 
         // Allow other tasks to run
