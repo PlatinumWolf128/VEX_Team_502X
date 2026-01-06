bc7e7d2ab2f6f7665ed0bed6911c9694a17b86bb
Sanjay Nataraj <2886932@edu.burnabyschools.ca>
2026-01-06 15:37:16 -0800

    Added more code to the test folder and also started adding code to the competition template.
    - Added and tested code to make the bot drive
      while field-orienting.
        - In other words, if you push the joystick
          forwards, the bot drives forwards relative
          to the driver, not relative to the bot.
        - So if the bot is turned 90 degrees clockwise
          relative to the field, then when you push
          the joystick forwards it starts strafing to
          the left from its point of view. From your
          point of view, it starts going forwards.
    - Also started bringing in the code from the test
      folder to the competition template (Specter-X)

--- 2025/Specter-X/include/hardware.h
+++ 2025/Specter-X/include/hardware.h
@@ -1,16 +1,57 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
+using namespace vex;
+
+// The brain and controller objects.
+extern brain Brain;
+extern controller Controller;
+
+// The motor ports for the drivetrain motors.
+const int FRONT_LEFT_PORT = PORT12; 
+const int FRONT_RIGHT_PORT = PORT13;
+const int BACK_LEFT_PORT = PORT14;
+const int BACK_RIGHT_PORT = PORT19;
+
+// The motors for the drivetrain.
+extern motor FrontLeft;
+extern motor FrontRight;
+extern motor BackLeft;
+extern motor BackRight;
+
+// This is the deadzone value. If the joystick's position along an axis is less
+// than or equal to this value, then a value of zero is returned for that axis
+// instead. 
+// This is done because the joysticks are imperfect and don't always
+// return to position zero, so this establishes a range of position values that
+// act as position zero.
+const double DEADZONE = 3;
+
+// The possible states for the drivetrain's alignment system.
+enum AlignmentStatus {NORTH,
+                      SOUTH,
+                      MAINTAIN_CURRENT,
+                      NEUTRAL};
+
+/**
+ * Controls the drivetrain using the holonomic drive formula and the values for
+ * forwards/backwards velocity, strafing velocity, and angular velocity that are
+ * passed in. 
+ */
+void robotOrientedDrive(double forward, double strafe, double turn);
 
 
-
-
-
-
-
-
-
-
+/**
+ * Controls the drivetrain using the holonomic drive formula and the values for
+ * forwards/backwards velocity, strafing velocity, and angular velocity that are
+ * passed in. 
+ * 
+ * Unlike the other drive function however, this also takes a heading value and
+ * drives in such a way so that no matter which way the robot is facing, if the
+ * driver pushes the joystick forward, the robot drives forwards relative to the
+ * driver.
+ */
+void fieldOrientedDrive(double forward, double strafe, double turn, double currentHeading);
 
 
 #endif
--- 2025/Specter-X/src/hardware.cpp
+++ 2025/Specter-X/src/hardware.cpp
@@ -1,2 +1,36 @@
 #include "vex.h"
 #include "hardware.h"
+#include <algorithm>
+#include <cmath>
+
+// The drivetrain motors.
+motor FrontLeft(FRONT_LEFT_PORT, ratio6_1, true);
+motor FrontRight(FRONT_RIGHT_PORT, ratio6_1);
+motor BackLeft(BACK_LEFT_PORT, ratio6_1, true);
+motor BackRight(BACK_RIGHT_PORT, ratio6_1);
+
+void robotOrientedDrive(double forward, double strafe, double turn) {
+
+    // Applying the deadzone
+    if (fabs(forward) <= DEADZONE) forward = 0;
+    if (fabs(strafe) <= DEADZONE) strafe = 0;
+
+    // The holonomic drive formula
+    double frontLeftSpeed = forward + strafe + turn;
+    double frontRightSpeed = forward - strafe - turn;
+    double backLeftSpeed = forward - strafe + turn;
+    double backRightSpeed = forward + strafe - turn;
+
+    // If the motor speeds are zero, apply the brakes.
+    if (fabs(frontLeftSpeed) == 0) FrontLeft.stop(brake);
+    if (fabs(frontRightSpeed) == 0) FrontRight.stop(brake);
+    if (fabs(backLeftSpeed) == 0) BackLeft.stop(brake);
+    if (fabs(backRightSpeed) == 0) BackRight.stop(brake);
+
+    FrontLeft.spin(fwd, frontLeftSpeed, velocityUnits::pct);
+    FrontRight.spin(fwd, frontRightSpeed, velocityUnits::pct);
+    BackLeft.spin(fwd, backLeftSpeed, velocityUnits::pct);
+    BackRight.spin(fwd, backRightSpeed, velocityUnits::pct);
+
+
+}
